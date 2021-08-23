bc3fd4c6543fa2fcd12772dfffa396b79cc1cf08
Rayan Bersabal <rayanbersabal@gmail.com>
2021-08-23 14:57:13 +0700

    mengumpulkan tugas

---
+++ first.js
@@ -0,0 +1,13 @@
+let teks = "Halo"
+var nama = "Rayan"
+console.log(`${teks} ${nama}`)
+
+const kelas = 10
+console.log(`Kelas: ${kelas}`)
+
+const array = ["Rayan", 10, {nama: "Rayan", kelas: 10}]
+console.log(array)
+
+const objek = {kelas: ["frontend", "backend", "mobile"]}
+
+console.log(objek.kelas[2])
